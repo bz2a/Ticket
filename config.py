bffa965157d4536f2dfb84229f8d596741311e43
Yacine Charrak <93998195+bz2a@users.noreply.github.com>
2025-08-20 19:40:10 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+BOT_TOKEN = "MTQwMjk3NDE3NDgyNTE1MjYxMg.GYiLfN.HzGSeAxpgACDSR2bPbIfRBHvvVXTphuj8Tfkco" 
+MONGO_URI = "mongodb+srv://yacinechrk:Charrak1959!@cluster0.ueuabvu.mongodb.net/mryacine"
+SERVER_ID = 817153556124532776
+LOG_CHANNEL_ID = 1391529599334092821
